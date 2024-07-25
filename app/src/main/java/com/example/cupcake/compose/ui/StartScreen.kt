206685df4d232bbcd1bf4d1bbe5f610e1732336e
AndreiRookie <andrey.idem@gmail.com>
2024-07-25 13:14:36 +0400

    theme changed to NoActionBar; MainScreenframe added to StartScreen

--- app/src/main/java/com/example/cupcake/compose/ui/StartScreen.kt
+++ app/src/main/java/com/example/cupcake/compose/ui/StartScreen.kt
@@ -28,94 +28,96 @@ import com.example.cupcake.R
 fun StartScreen(
     onOrderButtonClick: (count: Int) -> Unit
 ) {
+    MainScreenFrame(title = stringResource(id = R.string.app_name)) {
 
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            imageVector = ImageVector.vectorResource(id = R.drawable.cupcake),
-            contentDescription = null,
-            contentScale = ContentScale.Inside,
+        Column(
             modifier = Modifier
-                .size(dimensionResource(id = R.dimen.image_size))
-                .padding(top = 8.dp)
-
-        )
-
-        Text(
-            text = stringResource(id = R.string.order_cupcakes),
-            color = colorResource(id = R.color.material_on_background_emphasis_medium),
-            style = MaterialTheme.typography.h4,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-
-        Button(
-            onClick = { onOrderButtonClick(1) },
-            shape = RoundedCornerShape(4.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.pink_600)),
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .defaultMinSize(
-                    dimensionResource(id = R.dimen.order_cupcake_button_width)
-
-                )
+                .padding(16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = stringResource(id = R.string.one_cupcake).uppercase(),
-                color = colorResource(id = R.color.white),
-                fontSize = with(
-                    LocalDensity.current
-                ) {
-                    dimensionResource(id = R.dimen.order_button_font_size).toSp()
-                }
-                )
-        }
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.cupcake),
+                contentDescription = null,
+                contentScale = ContentScale.Inside,
+                modifier = Modifier
+                    .size(dimensionResource(id = R.dimen.image_size))
+                    .padding(top = 8.dp)
 
-        Button(
-            onClick = { onOrderButtonClick(6) },
-            shape = RoundedCornerShape(4.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.pink_600)),
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .defaultMinSize(
-                    dimensionResource(id = R.dimen.order_cupcake_button_width)
-
-                )
-        ) {
-            Text(
-                text = stringResource(id = R.string.six_cupcakes).uppercase(),
-                color = colorResource(id = R.color.white),
-                fontSize = with(
-                    LocalDensity.current
-                ) {
-                    dimensionResource(id = R.dimen.order_button_font_size).toSp()
-                }
             )
-        }
 
-        Button(
-            onClick = { onOrderButtonClick(12) },
-            shape = RoundedCornerShape(4.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.pink_600)),
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .defaultMinSize(
-                    dimensionResource(id = R.dimen.order_cupcake_button_width)
-
-                )
-        ) {
             Text(
-                text = stringResource(id = R.string.twelve_cupcakes).uppercase(),
-                color = colorResource(id = R.color.white),
-                fontSize = with(
-                    LocalDensity.current
-                ) {
-                    dimensionResource(id = R.dimen.order_button_font_size).toSp()
-                }
+                text = stringResource(id = R.string.order_cupcakes),
+                color = colorResource(id = R.color.material_on_background_emphasis_medium),
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.padding(bottom = 16.dp)
             )
+
+            Button(
+                onClick = { onOrderButtonClick(1) },
+                shape = RoundedCornerShape(4.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.pink_600)),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .defaultMinSize(
+                        dimensionResource(id = R.dimen.order_cupcake_button_width)
+
+                    )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.one_cupcake).uppercase(),
+                    color = colorResource(id = R.color.white),
+                    fontSize = with(
+                        LocalDensity.current
+                    ) {
+                        dimensionResource(id = R.dimen.order_button_font_size).toSp()
+                    }
+                )
+            }
+
+            Button(
+                onClick = { onOrderButtonClick(6) },
+                shape = RoundedCornerShape(4.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.pink_600)),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .defaultMinSize(
+                        dimensionResource(id = R.dimen.order_cupcake_button_width)
+
+                    )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.six_cupcakes).uppercase(),
+                    color = colorResource(id = R.color.white),
+                    fontSize = with(
+                        LocalDensity.current
+                    ) {
+                        dimensionResource(id = R.dimen.order_button_font_size).toSp()
+                    }
+                )
+            }
+
+            Button(
+                onClick = { onOrderButtonClick(12) },
+                shape = RoundedCornerShape(4.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(R.color.pink_600)),
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .defaultMinSize(
+                        dimensionResource(id = R.dimen.order_cupcake_button_width)
+
+                    )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.twelve_cupcakes).uppercase(),
+                    color = colorResource(id = R.color.white),
+                    fontSize = with(
+                        LocalDensity.current
+                    ) {
+                        dimensionResource(id = R.dimen.order_button_font_size).toSp()
+                    }
+                )
+            }
         }
     }
 }
